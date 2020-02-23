a5f3fe7f0f755469e7a43aa7c531c3823e5866a4
Pruthvi <45816639+Pruthvi-Sanghavi@users.noreply.github.com>
2020-02-23 17:29:09 -0500

    Add files via upload

--- imgrep.py
+++ imgrep.py
@@ -2,17 +2,29 @@
 # Importing the libraries
 import cv2 as cv
 import numpy as np
+# import imgproc
 # import argparse
 
 # ap = argparse.ArgumentParser()
 # ap.add_argument("-i","--image",required = True, help = "image path")
 # args = vars(ap.parse_args())
 
-image_train = cv.imread('index.png', 1) 				# reads the training image
-image_test = cv.VideoCapture('tag0.mp4')			# reads the testing video
+# image_train = cv.imread('index1.png',1) 				# reads the training image 
+# image_test = cv.imread('index1.png',1)				# reads the testing video
 
+cap = cv.VideoCapture('Tag0.mp4')
 
-gray = cv.cvtColor(image_train, cv.COLOR_BGR2GRAY)	# changes the color space of an 'image' to grayscale     
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    cv.imshow('frame',gray)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# cap.release()
+# cv2.destroyAllWindows()   
 gray = np.float32(gray)							    # Converts the gray image to float32 format because corner Harris accepts image in float32 format.
 
 
@@ -23,9 +35,17 @@ gray = np.float32(gray)							    # Converts the gray image to float32 format be
         # blockSize - It is the size of neighbourhood considered for corner detection
         # ksize - Aperture parameter of Sobel derivative used.
         # k - Harris detector free parameter in the equation.
-dst = cv.cornerHarris(gray, 2, 3, 0.04)
+dst = cv.cornerHarris(gray,2,3,0.04)
+ret, dst = cv.threshold(dst,0.1*dst.max(),255,0)
+dst = np.uint8(dst)
+ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+corners = cv.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+for i in range(1, len(corners)):
+    print(corners[i])
 
-cv.imshow('dst_image', dst)
-image_train[dst > 0.01*dst.max()] = [0, 0, 255]		# Threshold for an optimal value, it may vary depending on the image.
-cv.imshow('dst', image_train)					# Displays the image
+frame[dst>0.01*dst.max()]=[0,0,255]		# Threshold for an optimal value, it may vary depending on the image.
+cv.imshow('dst',frame)					# Displays the image
+cap.release()
 cv.waitKey(0) 									# creates a wait
+# cv.imwrite("tag.png",image)  					# Saves the image to the folder with the name tag.png
