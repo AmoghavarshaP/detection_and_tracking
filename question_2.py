88f2bfb7a469b98d7b3f5b303bc5a62071de70ff
Pruthvi <45816639+Pruthvi-Sanghavi@users.noreply.github.com>
2020-02-26 22:31:05 -0500

    Edit

--- question_2.py
+++ question_2.py
@@ -1,241 +1,80 @@
-import cv2
-import argparse
 import numpy as np
-import os, sys
-from numpy import linalg as LA
-from numpy import linalg as la
+import cv2
+from matplotlib import pyplot as plt
+import copy
+import imutils
 import math
-from PIL import Image
-import random
-
-def video(img_array,size):
-    video=cv2.VideoWriter('video_cube.avi',cv2.VideoWriter_fourcc(*'DIVX'), 16.0,size)
-    #print(np.shape(img_array))
-    for i in range(len(img_array)):
-        video.write(img_array[i])
-    video.release()
-#---------------------------------------------------------------
+import time
+import numpy.linalg as la
+import numpy.linalg as LA
+from numpy.linalg import inv
+from numpy.linalg import norm
 
 
 
-
-def binary(gray_scale):
-	for i in range(gray_scale.shape[0]):
-		for j in range(gray_scale.shape[1]):
-			gray_scale[i][j]
-	return gray_scale
-# @brief To give 4 points in Edgedetection
-#
-#  @param Image and old_ctr points
-#
-#  @return corners
-#
-def Edgedetection(image,old_ctr):
-
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    blurred = cv2.medianBlur(gray,3)
-    (T, thresh) = cv2.threshold(blurred, 180, 255, cv2.THRESH_BINARY)
-    contours, hierarchy=cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    ctr=[]
-    for j, cnt in zip(hierarchy[0], contours):
-        cnt_len = cv2.arcLength(cnt,True)
-        cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len,True)
-        if cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt) and len(cnt) == 4  :
-            cnt=cnt.reshape(-1,2)
-            if j[0] == -1 and j[1] == -1 and j[3] != -1:
-                ctr.append(cnt)
-        #print(np.shape(ctr))
-        old_ctr=ctr
-    return ctr
+cap = cv2.VideoCapture("Tag0.mp4")
+src = cv2.imread("Lena.png")
+while True:
+	ret, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	smooth = cv2.GaussianBlur(gray, (5,5), 1)
+	(True, thresh) = cv2.threshold(smooth, 200, 255, cv2.THRESH_BINARY)
+	contour, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	
+ 	rel_h = []
+ 	for h in hierarchy[0]:
+ 		if h[3] != -1 and h[2] == -1 and h[0] == -1:
+ 			rel_h.append(h)
+ 			# print rel_h
+ 	rel_c = []
+ 	for c in rel_h[0]:
+ 		if cv2.contourArea(contour[c]) > 500:
+ 			rel_c.append(contour[c])
+ 			# print rel_c
+ 	for cntrs in rel_c:
+ 		epsilon = 0.01*cv2.arcLength(cntrs, 1)
+ 		approx = cv2.approxPolyDP(cntrs, epsilon, 1)
+ 		# print approx
 
 
-
-# @brief Calculate homography
-#
-#  @param source and destination
-#
-#  @return homography
-#
-def homography_calc(src,des):
-    #print(des)
-    #print(np.shape(des))
-    c1 = des[0,0]
-    c2 = des[0,1]
-    c3 = des[0,2]
-    c4 = des[0,3]
-
-    w1 = src[0]
-    w2 = src[1]
-    w3 = src[2]
-    w4 = src[3]
-
-    A=np.array([[w1[0],w1[1],1,0,0,0,-c1[0]*w1[0],-c1[0]*w1[1],-c1[0]],
-                [0,0,0,w1[0], w1[1],1,-c1[1]*w1[0],-c1[1]*w1[1],-c1[1]],
-                [w2[0],w2[1],1,0,0,0,-c2[0]*w2[0],-c2[0]*w2[1],-c2[0]],
-                [0,0,0,w2[0], w2[1],1,-c2[1]*w2[0],-c2[1]*w2[1],-c2[1]],
-                [w3[0],w3[1],1,0,0,0,-c3[0]*w3[0],-c3[0]*w3[1],-c3[0]],
-                [0,0,0,w3[0], w3[1],1,-c3[1]*w3[0],-c3[1]*w3[1],-c3[1]],
-                [w4[0],w4[1],1,0,0,0,-c4[0]*w4[0],-c4[0]*w4[1],-c4[0]],
-                [0,0,0,w4[0], w4[1],1,-c4[1]*w4[0],-c4[1]*w4[1],-c4[1]]])
-
-    #Performing SVD
-    u, s, vt = la.svd(A)
-
-            # normalizing by last element of v
-            #v =np.transpose(v_col)
-    v = vt[8:,]/vt[8][8]
-
-    req_v = np.reshape(v,(3,3))
-
-    return req_v
+	p2 = []
+	for a in approx:
+		p2.append([a[0][1], a[0][0]])
+	
 
 
-def Superimposing(ctr,image,src):
-    pts_dst = np.array(ctr,dtype=float)
-    pts_src = np.array([[0,0],[511, 0],[511, 511],[0,511]],dtype=float)
-    h = homography_calc(pts_src, pts_dst)
+	p1 = np.array([[0, 0], [200, 0], [0, 200], [200, 200]], dtype="float32")
+	A_Matrix = []
 
+	for points in range(len(p2)):
+		x_1, y_1 = p1[points]
+		x_2, y_2 = p2[points]
+		A_Matrix.append([[x_1, y_1, 1, 0, 0, 0, -x_2 * x_1, -x_2 * y_1, -x_2]])
+		A_Matrix.append([[0, 0, 0, x_1, y_1, 1, -y_2 * x_1, -y_2 * y_1, -y_2]])
+	A_Matrix = np.array(A_Matrix)
+	A = np.reshape(A_Matrix, (8, 9))
+	[_, _, V] = np.linalg.svd(A)
+	H = V[:, -1]
+	H1 = np.reshape(H, (3, 3))
+   
+###############################################################################################
+# code for cube plotting starts here
 
-    temp = cv2.warpPerspective(src, h,(image.shape[1],image.shape[0]));
-    cv2.fillConvexPoly(image, pts_dst.astype(int), 0, 16);
+	pts_dst = np.array(p2,dtype="float32")
+	pts_src = np.array([[0,0],[200, 0],[200, 200],[0,200]],dtype="float32")
+	# M, mask = cv2.findHomography(pts_src, pts_dst, cv2.RANSAC,5.0)
+	temp = cv2.warpPerspective(frame, H1,(frame.shape[1],frame.shape[0]));
+	cv2.fillConvexPoly(frame, pts_dst.astype(int), 0, 16);
+	frame = frame + temp;
+	# cv2.imshow("screen1",frame)
 
-    image = image + temp;
-
-    return image,h
-# @brief To find get grid of tag image
-#
-#  @param image , corners
-#
-#  @return tag image and gray scale image
-#
-def perspective_for_tag(ctr,image):
-    dst1 = np.array([
-        [0, 0],
-        [100, 0],
-        [100, 100],
-        [0, 100]], dtype = "float32")
-
-    M1,status = cv2.findHomography(ctr[0], p1)
-    warp1 = cv2.warpPerspective(image.copy(), M1, (100,100))
-    warp2=cv2.medianBlur(warp1,3)
-    #warp2= warp1-warp1_5
-
-    tag_image=cv2.resize(warp2, dsize=None, fx=0.08, fy=0.08)
-    return tag_image,warp2
-# @brief To find tag ID and tag image
-#
-#  @param image , corners
-#
-#  @return tag and tag_id
-#
-def Tag_id_detection(ctr,tag_image):
-    gray = cv2.cvtColor(tag_image,cv2.COLOR_BGR2GRAY)
-    pixel_value=binary(gray)
-    print(pixel_value,'Tag Value')
-    status=0
-    A_ctr=ctr[0][0]
-    #print(A_ctr,'ctr A')
-    B_ctr=ctr[0][1]
-    #print(B_ctr,'ctr B')
-    C_ctr=ctr[0][2]
-    #print(C_ctr,'ctr B')
-    D_ctr=ctr[0][3]
-    #print(D_ctr,'ctr C')
-    if (pixel_value[2,2] == 1):
-        L1=A_ctr
-        L2=B_ctr
-        L3=C_ctr
-        L4=D_ctr
-        status=0
-        one = pixel_value[4,4]
-        two = pixel_value[4,3]
-        three = pixel_value[3,3]
-        four = pixel_value[3,4]
-
-    elif pixel_value[5,2]==1:
-        L1=D_ctr
-        L2=A_ctr
-        L3=B_ctr
-        L4=C_ctr
-        status=1
-        one = pixel_value[3,4]
-        two = pixel_value[4,4]
-        three = pixel_value[4,3]
-        four = pixel_value[3,3]
-
-    elif pixel_value[5,5] == 1:
-        L1=C_ctr
-        L2=D_ctr
-        L3=A_ctr
-        L4=B_ctr
-        status=2
-        one = pixel_value[3,3]
-        two = pixel_value[3,4]
-        three = pixel_value[4,4]
-        four = pixel_value[4,3]
-
-    elif pixel_value[2,5] == 1:
-        L1=B_ctr
-        L2=C_ctr
-        L3=D_ctr
-        L4=A_ctr
-        status=3
-        one = pixel_value[4,3]
-        two = pixel_value[3,3]
-        three = pixel_value[3,4]
-        four = pixel_value[4,4]
-
-    else:
-        L1=A_ctr
-        L2=B_ctr
-        L3=C_ctr
-        L4=D_ctr
-        one = pixel_value[4,4]
-        two = pixel_value[4,3]
-        three = pixel_value[3,3]
-        four = pixel_value[3,4]
-
-
-    new_ctr=np.array([[L1,L2,L3,L4]])
-
-    #print(new_ctr,'new_ctr')
-
-    tag_id = four*8 + three*4 + two*2 + one*1
-    print('Tag id value will be',tag_id)
-    return new_ctr,tag_id
-# @brief To draw 3d cube
-#
-#  @param image , perspective points in camera frame
-#
-#  @return image of cube
-#
-def draw(img, imgpts):
-    imgpts = np.int32(imgpts).reshape(-1,2)
-    # draw ground floor in green
-    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,0,0),-3)
-    # draw pillars in blue color
-    for i,j in zip(range(4),range(4,8)):
-        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(150,150,150),3)
-        # draw top layer in red color
-        img = cv2.drawContours(img, [imgpts[4:]],-1,(255,0,0),3)
-    return img
-
-# @brief Calculates projection matrix
-#
-#  @param Homography
-#
-#  @return Projection matrix for 3D transformation
-#
-def Projection_mat(homography):
-   # homography = homography*(-1)
-   # Calling the projective matrix function
-   	K =np.array([[1406.08415449821,0,0],
-       [ 2.20679787308599, 1417.99930662800,0],
-       [ 1014.13643417416, 566.347754321696,1]])
-
-   	K = K.T
+# Defining the camera intinsic matrix
+	K = np.array([[1406.08415449821,0,0],
+       			  [ 2.20679787308599, 1417.99930662800,0],
+       			  [ 1014.13643417416, 566.347754321696,1]])
+	K = K.T
 	K_inv = np.linalg.inv(K)
-	transform = np.dot(K_inv, homography)
+	transform = np.dot(K_inv, H1 )
 	column_1 = np.asarray(transform[:,0]).T
 	column_2 = np.asarray(transform[:,1]).T
 	column_3 = np.asarray(transform[:,2]).T
@@ -252,79 +91,36 @@ def Projection_mat(homography):
    	rotation_3 = np.cross(rotation_1, rotation_2)
    	projection = np.stack((rotation_1, rotation_2, rotation_3, translation)).T
    	proj_mat = np.dot(K, projection)
-	return np.dot(K, projection)
+   	print proj_mat
 
-    #def Three_d_cube(K, homography):
-    #    return 0
-# @brief it draws cube on image
-#
-#  @param Projection matrix and iamge
-#
-#  @return None
-#
-def Cube3D(proj_mat,image):
-    axis = np.float32([[0,0,0,1],[0,512,0,1],[512,512,0,1],[512,0,0,1],[0,0,-512,1],[0,512,-512,1],[512,512,-512,1],[512,0,-512,1]])
-    Proj= np.matmul(axis,proj_mat.T)
-    # Normalize the matrix
-    Norm1 = np.divide(Proj[0],Proj[0][2])
-    Norm2 = np.divide(Proj[1],Proj[1][2])
-    Norm3 = np.divide(Proj[2],Proj[2][2])
-    Norm4 = np.divide(Proj[3],Proj[3][2])
-    Norm5 = np.divide(Proj[4],Proj[4][2])
-    Norm6 = np.divide(Proj[5],Proj[5][2])
-    Norm7 = np.divide(Proj[6],Proj[6][2])
-    Norm8 = np.divide(Proj[7],Proj[7][2])
+   	axis = np.float32([[0,0,0,1],[0,512,0,1],[512,512,0,1],[512,0,0,1],[0,0,-512,1],[0,512,-512,1],[512,512,-512,1],[512,0,-512,1]])
+   	Proj = np.matmul(axis,proj_mat.T)
+   
 
-    points = np.vstack((Norm1,Norm2,Norm3,Norm4,Norm5,Norm6,Norm7,Norm8))
-    final_2d=np.delete(points,2, axis=1)
-    draw(image,final_2d)
-    return image
-# @brief Main loop to run the code and video breaking
-#
-#  @param path of video, source of lena
-#
-#  @return image_array and size of image
-#
-def img_proc(path,src):
+   	n1 = np.divide(Proj[0],Proj[0][2])
+   	n2 = np.divide(Proj[1],Proj[1][2])
+   	n3 = np.divide(Proj[2],Proj[2][2])
+   	n4 = np.divide(Proj[3],Proj[3][2])
+   	n5 = np.divide(Proj[4],Proj[4][2])
+   	n6 = np.divide(Proj[5],Proj[5][2])
+   	n7 = np.divide(Proj[6],Proj[6][2])
+   	n8 = np.divide(Proj[7],Proj[7][2])
+   	points = np.vstack((n1,n2,n3,n4,n5,n6,n7,n8))
+   	
+   	final_2d = np.delete(points,2, axis=1)
+   	# draw(image,final_2d)
 
-    vidObj = cv2.VideoCapture(path)
-    count = 0
-    success = 1
-    img_array=[]
-
-    while (success):
-        if (count==0):
-            success, image = vidObj.read()
-        height,width,layers=image.shape
-
-        size = (width,height)
-        if (count==0):
-            old_corners=0
-        corners=Edgedetection(image,old_corners)
-        #img = cv2.drawContours(image, corners,0,(0,255,0),1)
-        if(len(corners)==0):
-            corners=old_corners
-
-        tag_image,Tag=perspective_for_tag(corners,image)
-        new_corners,tag_id=Tag_id_detection(corners,tag_image)
-
-
-        image,h=Superimposing(new_corners,image,src)
-        proj_mat=Projection_mat(h)
-        image=Cube3D(proj_mat,image)
-        old_corners=corners
-        count += 1
-        print('Number of frames is',count)
-        # cv2.imwrite('%d.jpg' %count,image)
-        img_array.append(image)
-        success, image = vidObj.read()
-
-    return img_array,size
-if __name__ == '__main__':
-
-    # Calling the function
-    src=cv2.imread('Lena.png')
-    #print(np.size(src))
-    Image,size=img_proc('Tag1.mp4',src)
-    video(Image,size)
-p1 = np.array([[0, 0], [199, 0], [199, 199], [0, 199]], dtype="float32")
+   	imgpts = np.int32(final_2d).reshape(-1,2)
+   	img = cv2.drawContours(frame, [imgpts[:4]],-1,(255,0,255),-3)
+   	for i,j in zip(range(4),range(4,8)):
+   		img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(0,0,0),3)
+        # draw top layer in red color
+        img = cv2.drawContours(img, [imgpts[4:]],-1,(255,0,0),3)
+    	cv2.imshow("try",img)
+ 
+    	# M = cv2.getPerspectiveTransform(pts_src,pts_dst)
+    	warped_img = cv2.warpPerspective(frame, H1, (200, 200))
+    	# cv2.imshow("Image_Warped", warped_img)
+	if cv2.waitKey(1) & 0xff == ord("q"):
+		break
+cap.release()
